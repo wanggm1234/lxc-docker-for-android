265fa8b27f692d3a051e66bf49f22859caeb3a86
kuaiyun <42985334+Weiunli@users.noreply.github.com>
2023-10-31 02:39:07 +0800

    Update runcpatch.sh

--- runcpatch.sh
+++ runcpatch.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 RUNC_ADD_FILE=$1
-row=$(sed -n -e '/^static int cgroup_add_file/=' $RUNC_ADD_FILE)
-touch test2.txt
-sed -n -e '/static int cgroup_add_file/,/return 0/p' $RUNC_ADD_FILE >> test2.txt
-row2=$(sed -n -e '/return 0/=' test2.txt)
-row3=$(echo `expr $row + $row2 - 1`)
-sed -i "$row3 i\        }" $RUNC_ADD_FILE
-sed -i "$row3 i\                kernfs_create_link(cgrp->kn, name, kn);" $RUNC_ADD_FILE
-sed -i "$row3 i\                snprintf(name, CGROUP_FILE_NAME_MAX, \"%s.%s\", cft->ss->name, cft->name);" $RUNC_ADD_FILE
-sed -i "$row3 i\        if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {" $RUNC_ADD_FILE
+awk '/^static int cgroup_add_file/ { print NR; exit }' "$RUNC_ADD_FILE" > row.txt
+awk '/return 0/ { print NR; exit }' "$RUNC_ADD_FILE" > row2.txt
+row=$(cat row.txt)
+row2=$(cat row2.txt)
+row3=$(awk -v r1="$row" -v r2="$row2" 'BEGIN { print r1 + r2 - 1 }')
+awk -v r="$row3" -v line1="        if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {" -v line2="                snprintf(name, CGROUP_FILE_NAME_MAX, \"%s.%s\", cft->ss->name, cft->name);" -v line3="                kernfs_create_link(cgrp->kn, name, kn);" -v line4="        }" 'NR == r { print line1 "\n" line2 "\n" line3 "\n" line4 } 1' "$RUNC_ADD_FILE" > temp.txt
+mv temp.txt "$RUNC_ADD_FILE"
+rm row.txt row2.txt
